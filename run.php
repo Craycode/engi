b5630b0fa0f953f44744c51227a3b2727a282b0f
Piotr Suwik <piotr.suwik@kiddivouchers.com>
2013-07-03 02:30:27 +0100

    include doctrine, declare services

--- run.php
+++ run.php
@@ -5,11 +5,14 @@ define('BASE_PATH', realpath(__DIR__));
 /**
  * Get bootstrap.
  */
+
+/* @var $container Symfony\Component\DependencyInjection\ContainerBuilder */
 $container = require BASE_PATH . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 /**
  * Run application.
  */
-$container
-    ->get('application')
-    ->run();
+
+/* @var $application Engi\Application */
+$application = $container->get('application');
+$application->run();
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -17,6 +17,8 @@ define('PATH_APPLICATION', realpath(__DIR__ . DIRECTORY_SEPARATOR . '../'));
 define('PATH_SRC', PATH_APPLICATION . DIRECTORY_SEPARATOR . 'src');
 define('PATH_CLASSES', PATH_SRC . DIRECTORY_SEPARATOR . 'classes');
 define('PATH_CONFIG', PATH_SRC . DIRECTORY_SEPARATOR . 'config');
+define('PATH_MODELS', PATH_SRC . DIRECTORY_SEPARATOR . 'Engi' . DIRECTORY_SEPARATOR . 'Models');
+define('PATH_DB', PATH_APPLICATION . DIRECTORY_SEPARATOR . 'db');
 
 /**
  * Require autoloader.
@@ -27,6 +29,8 @@ use Engi\Components\DependencyInjection\ServicesExtension;
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
 
 /**
  * Class loading.
@@ -59,6 +63,18 @@ $container->registerExtension($extension);
 
 // Load extensions.
 $container->loadFromExtension($extension->getAlias());
+
+// Doctrine.
+$entityManager = EntityManager::create(
+        array(
+        'driver' => 'pdo_mysql',
+        'user' => 'application',
+        'password' => 'application',
+        'dbname' => 'engi',
+        ), Setup::createAnnotationMetadataConfiguration(array(PATH_MODELS), true)
+);
+
+$container->set('em', $entityManager);
 
 // Compile container.
 $container->compile();
