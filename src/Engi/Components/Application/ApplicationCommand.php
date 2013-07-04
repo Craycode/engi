2dc6ce40736994befba15c5c6db7500c430b89a6
Piotr Suwik <piotr.suwik@kiddivouchers.com>
2013-07-04 20:32:27 +0100

    inject container to command

--- src/Engi/Components/Application/ApplicationCommand.php
+++ src/Engi/Components/Application/ApplicationCommand.php
@@ -3,8 +3,21 @@
 namespace Engi\Components\Application;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\Container;
 
 class ApplicationCommand extends Command
 {
+	/**
+	 * Service container.
+	 * 
+	 * @var Container
+	 */
+	protected $_container;
 
+	public function __construct(Container $container, $name = null)
+	{
+		$this->_container = $container;
+
+		parent::__construct($name);
+	}
 }
--- src/Engi/Components/Application/Commands/HelloCommand.php
+++ src/Engi/Components/Application/Commands/HelloCommand.php
@@ -2,17 +2,22 @@
 
 namespace Engi\Components\Application\Commands;
 
-use Engi\Components\Application;
+use Engi\Application;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Engi\Components\Application\ApplicationCommand;
 
 class HelloCommand extends ApplicationCommand
 {
+
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$name = $input->getArgument('name');
 
-		$output->writeln(sprintf("Hello %s! How are you?", $name ?: 'buddy'));
+		/* @var $app Application */
+		$app = $this->_container->get('application');
+		$cname = $app->getLongVersion();
+
+		$output->writeln(sprintf("Hello %s! How are you? My version is %s.", $name ? : 'buddy', $cname));
 	}
 }
